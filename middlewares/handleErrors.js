96bb30d194349dd1a50deadd670ffc57712afa87
Nargisi <st-ana2005@yandex.ru>
2022-10-24 11:10:27 +0300

    Change next on _

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res, _) => {
   res.status(err.statusCode).send({ message: err.message });
 };
